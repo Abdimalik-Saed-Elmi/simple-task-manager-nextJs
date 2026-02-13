65f9fbd4dc91b331024ea5f044f13bbeef79dc2f
abdimalik <abdimalik@gmail.com>
2026-02-13 15:49:34 +0300

    Fix Render CJS build

---
+++ backend/dist/src/controllers/task.controller.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteTask = exports.updateTask = exports.getTasks = exports.createTask = void 0;
+const prisma_1 = require("../lib/prisma");
+const createTask = async (req, res) => {
+    const { title } = req.body;
+    if (!title) {
+        return res.status(400).json({ error: "title required" });
+    }
+    const task = await prisma_1.prisma.task.create({
+        data: { title },
+    });
+    res.status(201).json(task);
+};
+exports.createTask = createTask;
+const getTasks = async (_req, res) => {
+    const tasks = await prisma_1.prisma.task.findMany({
+        orderBy: { createdAt: "desc" },
+    });
+    res.json(tasks);
+};
+exports.getTasks = getTasks;
+const updateTask = async (req, res) => {
+    const id = req.params.id;
+    const { completed } = req.body;
+    const task = await prisma_1.prisma.task.update({
+        where: { id },
+        data: { completed },
+    });
+    res.json(task);
+};
+exports.updateTask = updateTask;
+const deleteTask = async (req, res) => {
+    const id = req.params.id;
+    const task = await prisma_1.prisma.task.delete({ where: { id } });
+    res.status(200).json({ msg: "Deleted successfully", task });
+};
+exports.deleteTask = deleteTask;
